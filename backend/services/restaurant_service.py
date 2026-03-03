03578faa396e127dc9a1094a905252b20881e6ba
Grayson <144386276+Graysonzz@users.noreply.github.com>
2026-03-03 14:29:00 -0800

    Update restaurant_service

--- backend/services/restaurant_service.py
+++ backend/services/restaurant_service.py
@@ -1,13 +1,13 @@
 from backend.models.user.restaurant_owner_model import RestaurantOwner
 from backend.models.user.admin_model import Admin
-from backend.models.user.user_model import User
-from backend.repositories.restaurant_repository import RestaurantRepository
+from backend.models.restaurant.restaurant_model import Restaurant
+
 
 class RestaurantService:
     def __init__(self, restaurant_repository):
         self.restaurant_repository = restaurant_repository
 
-    def register_restaurant(self, user, restaurant_data: dict):
+    def register_restaurant(self, user, data: dict):
         """
         Feat2-FR1: Storing Information
         Checks if user is an admin or owner
@@ -16,9 +16,18 @@ class RestaurantService:
         if not (isinstance(user, RestaurantOwner) or isinstance(user, Admin)):
             return {"success": False, "error": "unathouraized"}
         
-        if not restaurant_data.get("name") or not restaurant_data.get("address"):
-            return {"success": False, "error": "Missing required fields"}
+        try:
+            new_restaurant = Restaurant(
+                name=data.get("name"),
+                open_time=data.get("open_time", "09:00"),
+                close_time=data.get("close_time", "21:00"),
+                distance_from_user=data.get("distance_from_user", 0.0),
+                menu=[]
+
+            )
+            new_id = self.restaurant_repository.create_restaurant(new_restaurant)
+            return {"success": True, "restaurant_id": new_id}
         
-        # Save to repository
-        new_id = self.restaurant_repository.create_restaurant(restaurant_data)
-        return {"success": True, "restaurant_id": new_id}
+        # Handle potential errors
+        except Exception as e:
+            return {"success": False, "error": str(e)}
