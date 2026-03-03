36212d36d78ba0e3d8c4952ea67033bc333b923f
Grayson <144386276+Graysonzz@users.noreply.github.com>
2026-03-03 14:40:32 -0800

    import error

--- backend/tests/restaurant/unit_tests/test_restaurant_service.py
+++ backend/tests/restaurant/unit_tests/test_restaurant_service.py
@@ -2,7 +2,7 @@ import pytest
 
 from backend.services.restaurant_service import RestaurantService
 from backend.models.user.restaurant_owner_model import RestaurantOwner
-from backend.models.user.customer_model import Customer
+from backend.models.user.customer import Customer
 from backend.models.user.admin import Admin
 
 class MockRepo:
