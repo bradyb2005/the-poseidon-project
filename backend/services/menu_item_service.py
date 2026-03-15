fae955fe9d4177a7686567cfac2c0a657dfdba75
john_doe <john@example.com>
2026-03-15 16:55:38 -0700

    Fixed merge errors

--- backend/services/menu_item_service.py
+++ backend/services/menu_item_service.py
@@ -15,6 +15,20 @@ class MenuService:
     def __init__(self, repository):
         self.repository = repository
 
+
+    # --- Helper method ---
+
+    def get_and_verify_owner(self, restaurant_id: int, owner_id: int):
+        restaurant = self.repository.get_by_id(restaurant_id)
+        if not restaurant:
+            raise EntityNotFoundError(
+                f"Restaurant with ID {restaurant_id} not found.")
+
+        if restaurant.get('owner_id') != owner_id:
+            raise PermissionError(
+                "Access denied: You do not own this restaurant.")
+        return restaurant
+
     def add_menu_item(
             self, owner_id: int, restaurant_id: str, item_data: dict):
         # Fear2-FR4: Add new item
@@ -55,42 +69,14 @@ class MenuService:
             raise EntityNotFoundError(f"Menu item {item_id} not found.")
         return True
 
-    # --- Helper method ---
-
-    def get_and_verify_owner(self, restaurant_id: str, owner_id: int):
-        restaurant = self.repository.get_by_id(restaurant_id)
-        if not restaurant:
-            raise EntityNotFoundError(
-                f"Restaurant with ID {restaurant_id} not found.")
-
-        if restaurant.get('owner_id') != owner_id:
-            raise PermissionError(
-                "Access denied: You do not own this restaurant.")
-        return restaurant
  
-    def update_item_availability(self, restaurant_id: int,
+    def update_item_availability(self, owner_id: int, restaurant_id: int,
                                  item_id: int, status: bool):
-        # Check if restaurant exists
-        restaurant = self.repository.get_restaurant_by_id(restaurant_id)
-        if not restaurant:
-            raise EntityNotFoundError(
-                f"Update failed: Restaurant with ID {
-                    restaurant_id} not found.")
+        self.get_and_verify_owner(restaurant_id, owner_id)
 
-        # Check if menu item exists
-        menu_item = self.repository.get_menu_item_by_id(item_id)
-        if not menu_item:
-            raise EntityNotFoundError(
-                f"Update failed: Menu Item with ID {item_id} not found.")
-
-        # Checks if menu item is in restaurant
-        if menu_item.restaurant_id != restaurant_id:
-            raise EntityNotFoundError(
-                f"Item {item_id} does not belong to Restaurant {
-                    restaurant_id}.")
-
-        # Update
-        menu_item.availability = status
-        self.repository.save(menu_item)
-
-        return menu_item
+        success = self.repository.update_menu_item_availability(restaurant_id, item_id, status)
+    
+        if not success:
+            raise EntityNotFoundError(f"Menu Item with ID {item_id} not found.")
+    
+        return True
