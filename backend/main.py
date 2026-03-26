5a22b0597ecd78b8de71b58a415577c36060c2fa
Anjana Ratnala <144310117+aratnala7@users.noreply.github.com>
2026-03-26 11:48:38 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from backend.routes import restaurant_router, search_routes
+#from backend.routes.user_routes import router as user_router
 
 app = FastAPI(title="The Poseidon Project")
 
 app.include_router(restaurant_router.router)
 app.include_router(search_routes.router)
+#app.include_router(user_router)
 
 @app.get("/")
 def root():
